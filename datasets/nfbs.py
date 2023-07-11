f5ac147295b68d3069f3010b51a60dd9e4e6941f
Marc Zoon <marc.zoon@vintus.nl>
2023-07-11 11:58:35 +0200

    changed nfbs dataset

--- datasets/nfbs.py
+++ datasets/nfbs.py
@@ -1,12 +1,14 @@
 import argparse
-import os
 import random
 import tarfile
 import tempfile
 
 import h5py
+import matplotlib.pyplot as plt
 import nibabel
 import numpy as np
+import torch
+import torchio as tio
 from tqdm import tqdm
 
 
@@ -20,17 +22,13 @@ def main():
     h5test = h5file.create_group("test")
 
     tf = tarfile.open(args.input_file, mode="r")
-    members = list(
-        random.shuffle(
-            [
-                i
-                for i in tf.getmembers()
-                if i.name.endswith(
-                    f"T1w{f'_{args.suffix}' if args.suffix else ''}.nii.gz"
-                )
-            ]
-        )
-    )
+    members = [
+        i
+        for i in tf.getmembers()
+        if i.name.endswith(f"T1w{f'_{args.suffix}' if args.suffix else ''}.nii.gz")
+    ]
+    random.shuffle(members)
+    members = list(members)
     members_train = members[: int(len(members) * split[0])]
     members_validate = members[
         int(len(members) * split[0]) : int(len(members) * (split[0] + split[1]))
@@ -43,19 +41,32 @@ def main():
 
 
 def process_split(tf: tarfile.TarFile, members: list, h5group: h5py.Group):
+    transforms = tio.transforms.Compose(
+        [
+            tio.transforms.ToCanonical(),
+        ]
+    )
     for member in tqdm(members):
         name = member.name.split("/")[1]
         subgroup = h5group.create_group(name)
         tf.extract(member, tmp)
-        image = nibabel.load(f"{tmp}/{member.name}").get_fdata()[None, ...]
-        label = np.zeros_like(image)
-        subgroup.create_dataset("image", data=image, compression="gzip")
+
+        image = tio.ScalarImage(f"{tmp}/{member.name}")
+        image = transforms(image)
+
+        indices = []
+        for i in range(256):
+            m = (image.data[0, ..., i] > 0).float()
+            pct = torch.sum(m) / (256**2)
+            if pct >= 0.05:
+                indices.append(i)
+
+        label = np.zeros_like(image.data)
+        subgroup.create_dataset("image", data=image.data, compression="gzip")
         subgroup.create_dataset("label", data=label, compression="gzip")
 
         # Slice ids for axial view
-        subgroup.create_dataset(
-            "valid_axial", data=list(range(40, 101)), compression="gzip"
-        )
+        subgroup.create_dataset("healthy_axial", data=indices, compression="gzip")
 
 
 def create_argparser() -> argparse.ArgumentParser:
